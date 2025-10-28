2a6a96283f582c3e75ef277e38437924929142b8
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-28 15:04:14 +0530

    function add

--- src/pages/users/UserList.jsx
+++ src/pages/users/UserList.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get(
+        "https://express-mongo-connection-sigma.vercel.app/api/users"
+      );
+      const userData = res.data.users
+      console.log(userData);
+      setUsers(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>UserList</div>
-  )
+    <>
+      <div className="content-container">
+        <h1>All Users</h1>
+      </div>
+
+      <table border={1} cellPadding={5} cellSpacing={0} align="center">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>isActive</th>
+            <th>CreatedAt</th>
+            <th>UpdatedAt</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.isActive ? "Yes" : "No"}</td>
+              <td>{new Date(user.createdAt).toLocaleString("en-US" , {
+                day: "numeric",
+                month:"2-digit",
+                year:"numeric",
+                hour:"numeric",
+                minute:"numeric",
+                weekday:"long"
+              })}</td>
+               <td>{new Date(user.createdAt).toLocaleString("en-US" , {
+                day: "numeric",
+                month:"2-digit",
+                year:"numeric",
+                hour:"numeric",
+                minute:"numeric",
+                weekday:"long"
+              })}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default UserList
+export default UserList;
