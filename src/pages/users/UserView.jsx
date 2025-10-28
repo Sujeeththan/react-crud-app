bd4ea46933afee1ed3d73f3e4d90f5978365bcd2
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-28 15:58:19 +0530

    {id} get getOneUser function if(!user) return <p>Loading... </p> <ul> <li> </li> </ul>

--- src/pages/users/UserView.jsx
+++ src/pages/users/UserView.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function UserView() {
+    const [user, setuser] = useState()
+    const {id}  = useParams();
+
+     useEffect(() => {
+        getOneUser()
+     },[])
+
+    const getOneUser = async () => {
+        const res = await axios.get(`https://express-mongo-connection-sigma.vercel.app/api/users/${id}`)
+       const data = res.data.user
+        console.log(data);
+        setuser(data)
+    };
+    if(!user) return <p>Loading...</p>;
+    
   return (
-    <div>UserView</div>
+    <div>
+        <h1>UserView</h1>
+        <ul>
+            <li><strong>Name:</strong> {user.name}</li>
+            <li><strong>Email:</strong>{user.email}</li>
+            <li><strong>IsActive:</strong>{user.isActive ? "Yes" : "No"}</li>
+        </ul>
+    </div>
   )
 }
 
