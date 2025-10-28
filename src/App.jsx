9273d29fb44a2881b05925d0ba8f90dd218766e4
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-28 14:56:34 +0530

    import userForm import userView import userModel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import { Link, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import UserList from './pages/users/UserList'
+import UserForm from './pages/users/UserForm'
+import UserView from './pages/users/UserView'
+import UserModal from './pages/UserModal'
 
 function App() {
 
@@ -10,6 +13,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Navigate to="/users" />} />
         <Route path='/users' element={<UserList />} />
+        <Route path='/users/new' element={<UserForm />} />
+        <Route path='/users/:id/edit' element={<UserForm />} />
+        <Route path='/users/:id' element={<UserView />} />
+        <Route path='/user-model' element={<UserModal />} />
+
       </Routes>
     </>
   )
